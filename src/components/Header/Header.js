4b99d9d71dfc42f8ec04d46a967d39228a33bb00
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-10-27 15:15:54 +0900

    headerレイアウト

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import Style from './Header.module.scss'
 
 const Header = () => {
   return (
-    <div>
-      <div>
+    <div className={Style.header}>
+      <div className={Style.item}>
         <Link to="/">VideoTube</Link>
       </div>
-      <div>
+      <div className={Style.item}>
         <form>
           <input type="text" placeholder="検索" />
           <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
