1c195c2a5a26badf91f5a74fb50fab80d5db8685
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-10-28 12:21:49 +0900

    取得したリストの表示

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './store/index';
+import 'normalize.css'
 
 ReactDOM.render(
   <React.StrictMode>
