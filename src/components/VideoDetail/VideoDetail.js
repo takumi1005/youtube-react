603183723415271d10b1f691cef4ffa01636cefa
takumi1005 <53643382+takumi1005@users.noreply.github.com>
2020-10-28 15:46:23 +0900

    VideoPlayerの作成

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -2,6 +2,9 @@ import React, { useEffect, useContext } from 'react'
 import { useLocation } from 'react-router-dom'
 import { fetchSelectData } from '../../apis/index'
 import { Store } from '../../store/index'
+import VideoPlay from '../VideoPlay/VideoPlay'
+import Style from './VideoDetail.module.scss'
+import Linkify from 'react-linkify'
 
 const VideoDetail = () => {
   const { globalState, setGlobalState } = useContext(Store)
@@ -18,11 +21,16 @@ const VideoDetail = () => {
   useEffect(() => {
     setSelectedVideo()
   }, [])
-  return (
-    <div>
-      
+  return globalState.selected  && globalState.selected.id ? (
+    <div className={Style.wrap}>
+      <VideoPlay id={globalState.selected.id} />
+      <p>{globalState.selected.snippet.title}</p>
+      <hr />
+      <Linkify>
+        <pre>{globalState.selected.snippet.description}</pre>
+      </Linkify>
     </div>
-  )
+  ) : (<span>no data</span>)
 }
 
 export default VideoDetail
